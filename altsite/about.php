697b361fb253c42ae0be4918e3586559351f75c0
Aaron Dolgin <mydrum9@gmail.com>
2013-11-28 12:00:18 -0800

    fixed header and footer

---
+++ altsite/about.php
@@ -0,0 +1,11 @@
+<?php //include 'includes/overall/header.php'; ?>    
+
+<!--DO_NOT_TOUCH_THE_LINE_ABOVE_ENTER_TEXT_BELOW_THIS_LINE-->
+        
+      <p><strong>About Us </strong></p>
+      <p>Project 691 is a student lead mentor guided team of High school students that annually compete in the First Robotics Competition. In the Competition, students from different high schools around the world are faced off against each other in a sport like event that changes every year. Project 691 strives in giving students the tools they need to succeed in their future careers. Going beyond mechanical engineering, we teach programming, business, marketing, and electronics. </p>
+      <p><br />
+        Project 691 is completely sponsor supported. If you would like to donate, you can follow the donate tab on our website. If you would like to see a list of our generous sponsors, we have a tab for them too. </p>
+        
+<!--DO_NOT_TOUCH_THE_LINE_BELOW_ENTER_TEXT_ABOVE_THIS_LINE-->
+<?php //include 'includes/overall/footer.php'; ?> 
