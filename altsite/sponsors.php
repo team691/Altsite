12356edcec9a0d0dac49ade4ef28db6fea66de75
Aaron Dolgin <mydrum9@gmail.com>
2013-11-28 16:10:29 -0800

    fixed sponsors page

--- altsite/sponsors.php
+++ altsite/sponsors.php
@@ -1,26 +1,28 @@
 <?php include 'includes/overall/header.php'; ?>    
 
 <!--DO_NOT_TOUCH_THE_LINE_ABOVE_ENTER_TEXT_BELOW_THIS_LINE-->
-<p>
 <strong>Sponsors</strong>
-</p>
-<table border="1" class="content-table">
-	<tr>
-		<td><img class="icon" src="images/sponsors/baylessengineering.png"><br><a href="http://baylessengineering.com">Bayless Engineering</a></td>
-		<td><img class="icon" src="images/sponsors/nasa.png"><br><img class="icon" src="images/sponsors/jpl.png"><br><a href="http://jpl.nasa.gov">NASA JPL</a></td>
-		<td><img class="icon" src="images/sponsors/stjude.png"><br><a href="http://sjmfoundation.com">St. Jude Medical Founation</a></td>
-	</tr>
-	<tr>
-		<td><img class="icon" src="images/sponsors/raytheon.jpg"><br><a href="http://raytheon.com">Raytheon</a></td>
-		<td></td>
-		<td><img class="icon" src="images/sponsors/ndep.png"><br><a href="http://ndep.nih.gov">NDEP</a></td>
-	</tr>
-	<tr>
-		<td><img class="icon" src="images/sponsors/appliedcompanies.png"><br><a href="http://appliedcompanies.net">Applied Companies</a></td>
-		<td><img class="icon" src="images/sponsors/airgas.png"><br><a href="http://airgas.com">Airgas</a></td>
-		<td><img class="icon" src="images/sponsors/scmg.png"><br><a href="http://thescmg.com">SCMG</a></td>
-	</tr>
+<table width="900" border="0" align="center">
+  <tr>
+    <td width="218" align="center"><p><img src="images/sponsors/baylessengineering.png" align="top"><br>
+          <a href="http://baylessengineering.com">Bayless Engineering</a></p></td>
+    <td width="368" align="center"><img src="images/sponsors/nasa.png" width="100"><br><img src="images/sponsors/jpl.png" width="152" height="45"><br><a href="http://jpl.nasa.gov">NASA JPL</a></td>
+    <td width="292" align="center"><img src="images/sponsors/stjude.png" width="292" height="46"><a href="http://sjmfoundation.com">St. Jude Medical Founation</a></td>
+  </tr>
+  <tr>
+    <td align="center"><p><img src="images/sponsors/ndep.png"></p>
+      <p><a href="http://ndep.nih.gov">NDEP</a><br>
+      </p></td>
+    <td colspan="2" align="center"><img src="images/sponsors/raytheon.jpg"><br>      <a href="http://raytheon.com">Raytheon</a></td>
+  </tr>
+  <tr>
+    <td align="center"><p><img src="images/sponsors/appliedcompanies.png"></p>
+      <p><a href="http://appliedcompanies.net">Applied Companies</a></p></td>
+    <td align="center"><p><img src="images/sponsors/airgas.png"></p>
+      <p><a href="http://airgas.com">Airgas</a></p></td>
+    <td align="center"><p><img src="images/sponsors/scmg.png"></p>
+      <p><a href="http://thescmg.com">SCMG</a></p></td>
+  </tr>
 </table>
-
-<!--DO_NOT_TOUCH_THE_LINE_BELOW_ENTER_TEXT_ABOVE_THIS_LINE-->
+<p>&nbsp;</p>
 <?php include 'includes/overall/footer.php'; ?> 
