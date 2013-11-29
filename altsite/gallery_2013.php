031bfef49db5bf74ad199ff9057dbd9582fcf60e
Aaron Dolgin <mydrum9@gmail.com>
2013-11-28 20:27:10 -0800

    added gallery_2013.php

---
+++ altsite/gallery_2013.php
@@ -0,0 +1,103 @@
+<?php include 'includes/overall/header.php'; ?>    
+<!--DO_NOT_TOUCH_THE_LINE_ABOVE_ENTER_TEXT_BELOW_THIS_LINE-->
+ <div id="gallery1" class="lbGallery">
+  <ul>
+    <li> 
+    	<a href="images/2013/build_season/b/g01.jpg" title="name 1"> 
+        <img src="images/2013/build_season/s/g01.jpg" width="72" height="72" alt="alt name 1" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g02.jpg" title="name 2"> 
+    	<img src="images/2013/build_season/s/g02.jpg" width="72" height="72" alt="alt name 2" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g03.jpg" title=""> 
+        <img src="images/2013/build_season/s/g03.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g04.jpg" title=""> 
+        <img src="images/2013/build_season/s/g04.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g05.jpg" title=""> 
+        <img src="images/2013/build_season/s/g05.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g06.jpg" title=""> 
+        <img src="images/2013/build_season/s/g06.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g07.jpg" title=""> 
+        <img src="images/2013/build_season/s/g07.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g08.jpg" title=""> 
+        <img src="images/2013/build_season/s/g08.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g09.jpg" title=""> 
+        <img src="images/2013/build_season/s/g09.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g10.jpg" title=""> 
+        <img src="images/2013/build_season/s/g10.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g11.jpg" title=""> 
+        <img src="images/2013/build_season/s/g11.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g12.jpg" title=""> 
+        <img src="images/2013/build_season/s/g12.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g13.jpg" title=""> 
+        <img src="images/2013/build_season/s/g13.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g14.jpg" title=""> 
+        <img src="images/2013/build_season/s/g14.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g15.jpg" title=""> 
+        <img src="images/2013/build_season/s/g15.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g16.jpg" title=""> 
+        <img src="images/2013/build_season/s/g16.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g17.jpg" title=""> 
+        <img src="images/2013/build_season/s/g17.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g18.jpg" title=""> 
+        <img src="images/2013/build_season/s/g18.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g19.jpg" title=""> 
+        <img src="images/2013/build_season/s/g19.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g20.jpg" title=""> 
+        <img src="images/2013/build_season/s/g20.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    <li> 
+    	<a href="images/2013/build_season/b/g21.jpg" title=""> 
+        <img src="images/2013/build_season/s/g21.jpg" width="72" height="72" alt="" /> </a> 
+    </li>
+    
+  </ul>
+</div>
+<script type="text/javascript">
+// BeginWebWidget lightbox_gallery
+
+		$(function() {
+        $('#gallery1 a').lightBox();
+    });
+		
+
+// EndWebWidget lightbox_gallery
+</script>
+<!--DO_NOT_TOUCH_THE_LINE_BELOW_ENTER_TEXT_ABOVE_THIS_LINE-->
+<?php include 'includes/overall/footer.php'; ?> 
